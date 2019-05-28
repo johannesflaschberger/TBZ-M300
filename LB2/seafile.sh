ca248d449dcf3b17079d451291fdcb9b81136fdc
johannesflaschberger <johannes.flaschberger@gmx.ch>
2019-05-28 17:04:36 +0200

    Mit Frontend Provisionsskript implementation starten

--- LB2/seafile.sh
+++ LB2/seafile.sh
@@ -1,1 +1,23 @@
-echo "Seafile"
+#!/bin/bash
+
+# Updates herunterladen
+sudo yum update -y
+
+# Wget herunterladen
+sudo yum install -y wget
+
+# Benötigte Repositories herunterladen und installieren
+sudo yum install epel-release -y
+
+# Ufw installieren und einrichten
+sudo yum install ufw -y
+echo "y" | sudo ufw enable
+sudo ufw allow from 10.0.2.2 to any port 22
+echo "y" | sudo ufw delete 1    # Delete default allow any ssh
+echo "y" | sudo ufw delete 3    # Delete default allow any sshv6
+sudo ufw reload
+
+# Delete DB-Password file
+sudo rm -rf /etc/profile.d/db-passwd.sh
+
+echo "Database setup script complete!"
